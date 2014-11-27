3ea8d90faee38a400948f0b9d1f5b5ad297297f7
Peter Kawiak <peter.kawiak@algomi.com>
2014-11-27 01:42:54 +0000

    reverted

--- require.conf.js
+++ require.conf.js
@@ -2,14 +2,14 @@ require.config({
     baseUrl: './',
 
     paths: {
-        jquery: '/lib/jquery-2.1.1.min',
-        ConversionModule: '/src/modules/conversion/main',
-        ConversionView: '/src/modules/conversion/view',
-        ConversionModel: '/src/modules/conversion/model',
-        ConversionModelValidator: '/src/modules/conversion/validator',
-        ConversionController: '/src/modules/conversion/controller',
-        DigitsExtractor: '/src/modules/conversion/digitsExtractor',
-        Converter: '/src/modules/conversion/converter'
+        jquery: '../lib/jquery-2.1.1.min',
+        ConversionModule: '../src/modules/conversion/main',
+        ConversionView: '../src/modules/conversion/view',
+        ConversionModel: '../src/modules/conversion/model',
+        ConversionModelValidator: '../src/modules/conversion/validator',
+        ConversionController: '../src/modules/conversion/controller',
+        DigitsExtractor: '../src/modules/conversion/digitsExtractor',
+        Converter: '../src/modules/conversion/converter'
     }
 
 });
